2bde1d8e2747ec9e8a1893b3bcb7df614ae5f49f
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2014-09-24 10:20:24 +0200

    separate test from Makefile

---
+++ test.mk
@@ -0,0 +1,18 @@
+UNIT_TESTS_DIR := test/unit
+UNIT_TESTS_BUILD_DIR := tmp/test/unit
+UNIT_TESTS_SRC := $(shell find $(UNIT_TESTS_DIR) -name "*.cpp")
+UNIT_TESTS_OBJ := $(addprefix $(UNIT_TESTS_BUILD_DIR)/, $(patsubst $(UNIT_TESTS_DIR)/%,%,$(patsubst %.cpp,%.o,$(UNIT_TESTS_SRC))))
+UNIT_TESTS_EXE := tmp/test/unit/test-all
+
+$(UNIT_TESTS_BUILD_DIR)/%.o: $(UNIT_TESTS_DIR)/%.cpp
+	mkdir -p $(dir $@)
+	$(CXX) $(CXXFLAGS) -c -o $@ $<
+
+$(UNIT_TESTS_EXE): $(UNIT_TESTS_OBJ) $(OBJS)
+	$(CXX) -o $@ -lgtest -lgmock $(LDFLAGS) $(UNIT_TESTS_OBJ) $(OBJS)
+
+unit_test: $(UNIT_TESTS_EXE)
+	cd test/mocks && ../../tmp/test/unit/test-all
+
+test: executables unit_test
+	for testScript in $(wildcard test/scripts/auto_*); do echo $$testScript; $$testScript; done
